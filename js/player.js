f1a1cc1a577bc001c50658a0e97f2e5493c823a5
Николай Гончаров <44123818+kolchindru@users.noreply.github.com>
2020-10-20 14:52:15 +0400

    figured out pdfs

--- js/player.js
+++ js/player.js
@@ -1,40 +1,6 @@
 function singleArticle(article) {
-    var pdfjsLib = window['pdfjs-dist/build/pdf'];
-    pdfjsLib.GlobalWorkerOptions.workerSrc = 'js/pdf.worker.js';
-
-    // Using DocumentInitParameters object to load binary data.
-    var loadingTask = pdfjsLib.getDocument(article['link']);
-    loadingTask.promise.then(function(pdf) {
-      console.log('PDF loaded');
-
-      // Fetch the first page
-      var pageNumber = 1;
-      pdf.getPage(pageNumber).then(function(page) {
-        console.log('Page loaded');
-
-        var scale = 1.5;
-        var viewport = page.getViewport({scale: scale});
-
-        // Prepare canvas using PDF page dimensions
-        var canvas = document.getElementById('mediaplayer');
-        var context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-
-        // Render PDF page into canvas context
-        var renderContext = {
-          canvasContext: context,
-          viewport: viewport
-        };
-        var renderTask = page.render(renderContext);
-        renderTask.promise.then(function () {
-          console.log('Page rendered');
-        });
-      });
-    }, function (reason) {
-      // PDF loading error
-      console.error(reason);
-    });
+    console.log("web/viewer.html?file=https://cors-anywhere.herokuapp.com/" + article['link']);
+    $("#mediaplayer").attr("src", "web/viewer.html?file=https://cors-anywhere.herokuapp.com/" + article['link']);
 }
 
 // Функция создания кнопки
